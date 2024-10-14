107917d5a1e81f152a2ae4c6db185da8043031cc
Thais Freire <156026093+thaisfreires@users.noreply.github.com>
2024-10-14 14:38:33 +0100

    AIMAID-8-fix (#6)

---
+++ routes/api.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+
+
+//Route::get('/user', function (Request $request) {
+    //return $request->user();
+//})->middleware('auth:sanctum');
+
+Route::post('/citizen/verify', [UserApiController::class, 'verify']);
+
+
